2724ed6e3862831d316d11a1d59f1c050f9ab1df
Amar Chatman <amarjerome@gmail.com>
2018-02-13 17:33:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ if array.length == 1
 elsif array.length == 2
   return array.join(" and ")
 elsif array.length >= 3
-   array[-1] = "and kiwi"
+   array[-1] = "and #{array[-1]}"
 end
 #binding.pry
 end
