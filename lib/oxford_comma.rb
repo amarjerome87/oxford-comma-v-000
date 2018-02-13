cbf178bc8a106d27607616b4bce71a6355d86520
Amar Chatman <amarjerome@gmail.com>
2018-02-13 17:24:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
 
 if array.length == 1
   return array[0]
-elsif array == (2)
+elsif array.length == 2
   return array.join("and")
 elsif array == (1..3)
   return array.join(",","and")
