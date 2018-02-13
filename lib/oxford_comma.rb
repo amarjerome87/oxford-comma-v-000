d0b8de2a7254837152345662129372e96153f90b
Amar Chatman <amarjerome@gmail.com>
2018-02-13 16:44:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
 
-if array == (1..array.length)
+if array == (1)
   return array.join(",")
-elsif array == (1..array.length)
+elsif array == (1..3)
   return array.join(",","and")
 end
 
